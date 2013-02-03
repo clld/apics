240bf99a413759f4592378d89352a9855c6efafc
Robert Forkel <xrotwang@googlemail.com>
2013-02-03 20:34:52 +0100

    work on deployment

---
+++ fabfile.py
@@ -0,0 +1,18 @@
+from fabric.api import task, hosts
+from fabric.contrib.console import confirm
+
+from clld.deploy import config, util
+
+
+APP = config.APPS['apics']
+
+
+@hosts('forkel@cldbstest.eva.mpg.de')
+@task
+def deploy_test():
+    util.deploy(APP, 'test')
+
+
+@task
+def deploy():
+    util.deploy(APP, 'production')
