58f65a1815a1b8150283ebfa19b1c47655f82886
Sebastian Bank <sebastian.bank@uni-leipzig.de>
2015-01-07 13:25:36 +0100

    remove outdated metadata

--- setup.py
+++ setup.py
@@ -1,10 +1,4 @@
-import os
-
 from setuptools import setup, find_packages
-
-here = os.path.abspath(os.path.dirname(__file__))
-README = open(os.path.join(here, 'README.txt')).read()
-CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
 
 requires = [
     'clld>=0.15.5',
@@ -21,7 +15,7 @@ requires = [
 setup(name='apics',
       version='0.0',
       description='apics',
-      long_description=README + '\n\n' + CHANGES,
+      long_description='',
       classifiers=[
         "Programming Language :: Python",
         "Framework :: Pyramid",
